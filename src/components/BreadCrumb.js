838c27278324bee6e2651eec79add6269f9062fc
dinoxas <dhassiotis07@gmail.com>
2020-06-28 15:06:37 +0100

    BreadCrumb and PageHero component

--- src/components/BreadCrumb.js
+++ src/components/BreadCrumb.js
@@ -1,11 +1,36 @@
 import React from "react"
+import PropTypes from "prop-types"
+import { Link } from "gatsby"
+import { BreadCrumbWrapper } from "./styles/BreadCrumbStyles"
 
-const BreadCrumb = () => {
+const BreadCrumb = ({ parent }) => {
+  // const { title, link } = parent
   return (
-    <div>
-      <h1>BreadCrumb</h1>
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-9 offset-lg-3">
+          <BreadCrumbWrapper>
+            <Link to="/">
+              <span>Tango Brand Alliance</span>
+            </Link>
+            <span className="divider">/</span>
+            {parent ? (
+              <>
+                <Link to={parent.link}>
+                  <span dangerouslySetInnerHTML={{ __html: parent.title }} />
+                </Link>
+                <span className="divider">/</span>
+              </>
+            ) : null}
+          </BreadCrumbWrapper>
+        </div>
+      </div>
     </div>
   )
+}
+
+BreadCrumb.propTypes = {
+  parent: PropTypes.object,
 }
 
 export default BreadCrumb
--- src/components/PageHero.js
+++ src/components/PageHero.js
@@ -1,11 +1,25 @@
 import React from "react"
+import PropTypes from "prop-types"
+import Img from "gatsby-image"
+import styled from "styled-components"
 
-const PageHero = () => {
-  return (
-    <div>
-      <h1>Page Hero</h1>
+// using gatsby image instead of normal img
+const StyledImg = styled(Img)`
+  height: 300px;
+`
+
+const PageHero = ({ img }) => (
+  <div className="container-fluid">
+    <div className="row">
+      <div className="col" style={{ padding: "0" }}>
+        <StyledImg fluid={img} />
+      </div>
     </div>
-  )
+  </div>
+)
+
+PageHero.propTypes = {
+  img: PropTypes.object,
 }
 
 export default PageHero
