32dbebc264ecb20046b66db249a4d76f06d967ec
dinoxas <dhassiotis07@gmail.com>
2020-06-29 13:17:38 +0100

    PostSidebar component

--- src/components/post/PostSidebar.js
+++ src/components/post/PostSidebar.js
@@ -1,11 +1,44 @@
 import React from "react"
+import PropTypes from "prop-types"
+import { Link } from "gatsby"
+import AuthorIcon from "../../images/tango-author-icon.svg"
+import CategoryIcon from "../../images/tango-category-icon.svg"
+import DateIcon from "../../images/tango-date-icon.svg"
+import { SidebarWrapper, SidebarMenu } from "./styles/PostSidebarStyles"
 
-const PostSidebar = () => {
-  return (
-    <div>
-      <h2>Post sidebar</h2>
-    </div>
-  )
+const PostSidebar = ({ date, author, categories }) => (
+  <SidebarWrapper className="col-lg-3">
+    <SidebarMenu>
+      <li className="sidebar-menu-header">
+        <img src={DateIcon} alt="Date icon" />
+        <span>{date.replace(/\W+/g, " ")}</span>
+      </li>
+      <li className="sidebar-menu-header">
+        <img src={AuthorIcon} alt="Author icon" />
+        <span>{author}</span>
+      </li>
+      <li className="sidebar-menu-header">
+        <img src={CategoryIcon} alt="Category icon" />
+        <span>Categories</span>
+      </li>
+      {categories.map((cat, i) =>
+        // for post with categories which dont belong to all-category
+        cat.slug !== "all-trendspaningar" ? (
+          <li key={cat.id} className="">
+            <Link to={`/trends/${cat.slug}/`}>
+              <span dangerouslySetInnerHTML={{ __html: cat.name }} />
+            </Link>
+          </li>
+        ) : null
+      )}
+    </SidebarMenu>
+  </SidebarWrapper>
+)
+
+PostSidebar.propTypes = {
+  date: PropTypes.string,
+  author: PropTypes.string,
+  categories: PropTypes.array,
 }
 
 export default PostSidebar
